cdc8a4b02e64984973beb716d00798a3f6ae410b
Octavia Cosby <159678136+OctaviaCosby@users.noreply.github.com>
2025-06-18 11:46:26 -0400

    added additional ibraries to ImageDetector.py

--- ImageDetector.py
+++ ImageDetector.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.image import imread
 import tensorflow_hub as hub
 from tensorflow.keras.applications import EfficientNetB0
+form tensorflow.keras.applications import VGG16
 from tensorflow.keras import layers, models
 
 
